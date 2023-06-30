8ccf79a21da5a3d00f76679bfbeeaafd03546400
jmatejcz <jakub.matejczyk.stud@pw.edu.pl>
2023-06-30 13:15:41 +0200

    separate to diffrent files

--- alpr.py
+++ alpr.py
@@ -6,16 +6,11 @@ import json
 import copy
 import time
 import torchvision
-from torchvision.utils import draw_bounding_boxes
 from torchvision.models.detection.faster_rcnn import (
     FastRCNNPredictor,
-    FasterRCNN_ResNet50_FPN_V2_Weights,
 )
-from torchvision import transforms as T
-import torchvision.transforms.functional as F
-import cv2
-import matplotlib.pyplot as plt
-from typing import Dict, List, Optional, Tuple, Union
+import utils
+
 
 
 #     ⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⣤⣤⣶⣦⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀
@@ -37,7 +32,7 @@ from typing import Dict, List, Optional, Tuple, Union
 # ⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⢹⣿⡆⠀⠀⠀⣸⣿⠇⠀⠀⠀
 # ⠀⠀⠀⠀⠀⠀⠀⢿⣿⣦⣄⣀⣠⣴⣿⣿⠁⠀⠈⠻⣿⣿⣿⣿⡿⠏⠀⠀⠀⠀
 # ⠀⠀⠀⠀⠀⠀⠀⠈⠛⠻⠿⠿⠿⠿⠋⠁⠀⠀
-def get_model_instance_segmentation(num_classes, default: bool):
+def get_model_instance_segmentation(num_classes: int, default: bool):
     if default:
         model = torchvision.models.detection.fasterrcnn_resnet50_fpn_v2(
             weights=None, weights_backbone="DEFAULT", trainable_backbone_layers=3
@@ -48,134 +43,11 @@ def get_model_instance_segmentation(num_classes, default: bool):
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     # replace the pre-trained head with a new one
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
-
     return model
 
 
-def show(imgs):
-    if not isinstance(imgs, list):
-        imgs = [imgs]
-    fig, axs = plt.subplots(ncols=len(imgs), squeeze=False)
-    for i, img in enumerate(imgs):
-        img = img.detach()
-        img = F.to_pil_image(img)
-        axs[0, i].imshow(np.asarray(img))
-        axs[0, i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
-    plt.show()
-
-
-def draw_bboxes(image, boxes, labels=None):
-    image = torch.as_tensor(image * 255, dtype=torch.uint8)
-    if boxes.shape[1] == 0:
-        return
-    else:
-        visual = draw_bounding_boxes(image, boxes, colors=(255, 0, 0), width=2)
-        show(visual)
-
-
-def save_boxes_img(path_to_save: str, image: torch.Tensor, boxes: torch.Tensor):
-    croped_img = image[:, int(boxes[1]) : int(boxes[3]), int(boxes[0]) : int(boxes[2])]
-    croped_img = F.to_pil_image(croped_img)
-    cv2.imwrite(path_to_save, np.asarray(croped_img))
-
-
-def _flip_coco_person_keypoints(kps, width):
-    flip_inds = [0, 2, 1, 4, 3, 6, 5, 8, 7, 10, 9, 12, 11, 14, 13, 16, 15]
-    flipped_data = kps[:, flip_inds]
-    flipped_data[..., 0] = width - flipped_data[..., 0]
-    # Maintain COCO convention that if visibility == 0, then x, y = 0
-    inds = flipped_data[..., 2] == 0
-    flipped_data[inds] = 0
-    return flipped_data
-
-
-class Compose:
-    def __init__(self, transforms):
-        self.transforms = transforms
-
-    def __call__(self, image, target):
-        for t in self.transforms:
-            image, target = t(image, target)
-        return image, target
-
-
-class RandomHorizontalFlip(T.RandomHorizontalFlip):
-    def forward(
-        self, image: torch.Tensor, target: Optional[Dict[str, torch.Tensor]] = None
-    ) -> Tuple[torch.Tensor, Optional[Dict[str, torch.Tensor]]]:
-        if torch.rand(1) < self.p:
-            image = F.hflip(image)
-            if target is not None:
-                _, _, width = F.get_dimensions(image)
-                target["boxes"][:, [0, 2]] = width - target["boxes"][:, [2, 0]]
-                if "masks" in target:
-                    target["masks"] = target["masks"].flip(-1)
-                if "keypoints" in target:
-                    keypoints = target["keypoints"]
-                    keypoints = _flip_coco_person_keypoints(keypoints, width)
-                    target["keypoints"] = keypoints
-        return image, target
-
-
-class PILToTensor(torch.nn.Module):
-    def forward(
-        self, image: torch.Tensor, target: Optional[Dict[str, torch.Tensor]] = None
-    ) -> Tuple[torch.Tensor, Optional[Dict[str, torch.Tensor]]]:
-        image = F.pil_to_tensor(image)
-        return image, target
-
-
-class ToTensor(object):
-    def __call__(self, image, target):
-        image = F.to_tensor(image)
-        return image, target
-
-
-class ConvertImageDtype(torch.nn.Module):
-    def __init__(self, dtype: torch.dtype) -> None:
-        super().__init__()
-        self.dtype = dtype
-
-    def forward(
-        self, image: torch.Tensor, target: Optional[Dict[str, torch.Tensor]] = None
-    ) -> Tuple[torch.Tensor, Optional[Dict[str, torch.Tensor]]]:
-        image = F.convert_image_dtype(image, self.dtype)
-        return image, target
-
-
-def get_transform(train: bool):
-    transforms = []
-    if train:
-        transforms.append(PILToTensor())
-        transforms.append(ConvertImageDtype(torch.float))
-        return Compose(transforms)
-    else:
-        transforms.append(T.PILToTensor())
-        transforms.append(T.ConvertImageDtype(torch.float))
-        return T.Compose(transforms)
-
-
-def get_IoU(box1: list, box2: list) -> float:
-    x_left = max(box1[0], box2[0])
-    y_top = max(box1[1], box2[1])
-    x_right = min(box1[0] + box1[2], box2[0] + box2[2])
-    y_bottom = min(box1[1] + box1[3], box2[1] + box2[3])
-    # If the intersection is non-existent (boxes don't overlap)
-    if x_right < x_left or y_bottom < y_top:
-        return 0.0
-    # Calculate the areas of the bounding boxes
-    box1_area = box1[2] * box1[3]
-    box2_area = box2[2] * box2[3]
-
-    intersection_area = (x_right - x_left) * (y_bottom - y_top)
-    union_area = box1_area + box2_area - intersection_area
-    iou = intersection_area / union_area
-
-    return iou
-
-
 class InstanceSegmentationDataset(torch.utils.data.Dataset):
-    def __init__(self, root, images_path, transforms=None):
+    def __init__(self, root, images_path, transforms):
         self.root = root
         self.transforms = transforms
         self.imgs = list(sorted(os.listdir(self.root + images_path)))[:10000]
@@ -188,10 +60,8 @@ class InstanceSegmentationDataset(torch.utils.data.Dataset):
             except:
                 print(f"could not open {file_img_path}")
 
-    def __getitem__(self, idx):
-        if self.transforms is not None:
-            img = self.transforms(self.ordered_images[idx])
-
+    def __getitem__(self, idx) -> torch.Tensor:
+        img = self.transforms(self.ordered_images[idx])
         return img
 
     def __len__(self):
@@ -352,7 +222,7 @@ class LicensePlatesDetection:
 
             if show_fr > 0:
                 if i % show_fr == 0:
-                    draw_bboxes(inputs[0], targets[0]["boxes"], targets[0]["labels"])
+                    utils.draw_bboxes(inputs[0], targets[0]["boxes"], targets[0]["labels"])
 
             self.optimizer.zero_grad()
             losses_dict = self.model(inputs, targets)
@@ -388,16 +258,16 @@ class LicensePlatesDetection:
                 for j, outs in enumerate(filtered_outputs):
                     if show_fr > 0:
                         if i % show_fr == 0:
-                            draw_bboxes(inputs[0], outs[0]["boxes"].unsqueeze(0))
+                            utils.draw_bboxes(inputs[0], outs[0]["boxes"].unsqueeze(0))
 
                     if save_boxes:
                         path = f"/workspace/alpr/croped_plates/{i}_{j}.jpg"
-                        save_boxes_img(path, inputs[0], outs[0]["boxes"])
+                        utils.save_boxes_img(path, inputs[0], outs[0]["boxes"])
 
                     if len(filtered_outputs) > 0:
                         iou_scores.append(
                             #TODO targets [?] zrobic zeby dla bathc wiecej niz 1 tez dzialalo
-                            get_IoU(outs[0]["boxes"], targets[0]["boxes"].cpu().numpy()[0])
+                            utils.get_IoU(outs[0]["boxes"], targets[0]["boxes"].cpu().numpy()[0])
                         )
                     else:
                         iou_scores = [0.0]
@@ -437,7 +307,7 @@ class AlprSetupPlateCrop:
         self.model = model
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.dataset = InstanceSegmentationDataset(
-            root=root, images_path=path_to_imgs, transforms=get_transform(False)
+            root=root, images_path=path_to_imgs, transforms=utils.get_transform(False)
         )
         self.dataloader = torch.utils.data.DataLoader(
             self.dataset,
@@ -457,7 +327,7 @@ class AlprSetupPlateCrop:
                 if len(outputs[0]["boxes"]) > 0:
                     best_box = outputs[0]['boxes'][0]
                     path_to_save_file = path_to_save + f'{i+50000}.jpg'
-                    save_boxes_img(path_to_save_file, inputs[0], best_box)
+                    utils.save_boxes_img(path_to_save_file, inputs[0], best_box)
             
                 if i%100 == 0:
                     time_end = time.time()
@@ -467,11 +337,3 @@ class AlprSetupPlateCrop:
 
     def load_state_dict(self, path_to_weights: str):
         self.model.load_state_dict(torch.load(path_to_weights))
-
-if __name__ == "__main__":
-    num_classes = 2
-    root = "/workspace/alpr/"
-    model = get_model_instance_segmentation(num_classes, default=True)
-    cropper = AlprSetupPlateCrop(root=root, model=model, path_to_imgs='frames/')
-    cropper.load_state_dict(path_to_weights=root+"fasterrcnn_resnet50_fpn_v2.pt")
-    cropper.crop_plates(path_to_save=root+'croped_plates/1/')
